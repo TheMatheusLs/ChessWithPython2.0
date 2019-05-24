d0937b879f7ace13083fcd1a4b42a58c1e780bb0
Matheus Lobo <matheusls@live.com>
2019-05-24 06:50:04 -0300

    Criando a classe posicao Xadrez

--- GameModule.py
+++ GameModule.py
@@ -1,5 +1,5 @@
 #Inteligência do jogo de xadrez, como as peças vão se movimentação. Mecanica do jogo
-from BoardModule import Piece       #Importa o módulo das peças
+from BoardModule import *           #Importa o módulo das peças
 
 class Tower(Piece):                 #Classe com as regras da 'torre' e herda da classe Peça
     
@@ -10,3 +10,30 @@ class King(Piece):                  #Classe com as regras da 'rei' e herda da cl
     
     def __str__(self):
         return 'K'                  #Imprime a 'Rei' como um 'R' 
+
+class ChessPositon:
+
+    def __init__(self, col, row):
+        self._col = col
+        self._row = row
+
+    @property
+    def col(self):
+        return self._col
+    @col.setter
+    def col(self,col):
+        self._col = col
+    
+    @property
+    def row(self):
+        return self._row
+    @row.setter 
+    def row(self, row):
+        self._row = row
+
+    def toPosition(self):
+        aux = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
+        return Position(8-self.row, aux[self.col.lower()])
+
+    def __str__(self):
+        return f'{self.col.upper()}{self.row}'     
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from BoardModule import Position, Board, Color, BoardException  #Importa os modulos do tabuleiro
-from ApplicationModule import Screen            #Importa o modulo de tela
+from BoardModule import *  #Importa os modulos do tabuleiro
+from ApplicationModule import *           #Importa o modulo de tela
 from GameModule import *                        #Importa os modulos do jogo
 
-try:
-    tab = Board(8,8)        #Cria um tabuleiro 8x8
+pos = ChessPositon('c',7)
 
-    tab.putPiece(Tower(tab,Color.preta),Position(0,0))  #Colaca uma torre preta no tabuleio na posição x= 0 e y= 0
-    tab.putPiece(Tower(tab,Color.preta),Position(1,9))  #Colaca uma torre preta no tabuleio na posição x= 1 e y= 3
-    tab.putPiece(King(tab,Color.preta),Position(0,2))   #Colaca um rei preto no tabuleio na posição x= 2 e y= 4
+print(pos)
 
-    Screen.PrintBoard(tab)      #Imprime o tabuleiro 'tab'
-except BoardException as e:
-    print(e)
+print(pos.toPosition())
